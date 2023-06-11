c0fb4e757028c7d0f4e2d29d5bb5e7ea2d2adb08
Simon.Chen <ice_mind@yeah.net>
2023-06-11 19:00:06 +0800

    update

--- miniprogram/pages/xiyu/xiyu.ts
+++ miniprogram/pages/xiyu/xiyu.ts
@@ -7,9 +7,9 @@ Component({
         title: "细语",
         userInfo: {avatar:""},
         background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
-        talks:[{imgUrl:'/assets/xinliao.png',url:'../../talks/pages/talks/xintalk',text:'心聊'},
-        {imgUrl:'/assets/talk.png',url:'../../talks/pages/talks/zhongle',text:'众乐'},
-        {imgUrl:'/assets/wenti.png',url:'../../talks/pages/talks/wenti',text:'解惑'}]
+        talks:[{imgUrl:'/assets/xinliao.png',url:'/pages/talks/xintalk',text:'心聊'},
+        {imgUrl:'/assets/talk.png',url:'/pages/talks/zhongle',text:'众乐'},
+        {imgUrl:'/assets/wenti.png',url:'/pages/talks/wenti',text:'解惑'}]
     },
     lifetimes:{
         ready:function(){
